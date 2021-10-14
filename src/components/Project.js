a006040b0a411aa69bc1c606db95699abb2e1968
pemba1s1 <norsangpemba@gmail.com>
2021-10-14 19:35:17 +0545

    added projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import { project,item1,item2,tech } from '../styles/projects.module.css'
 import 'boxicons'
+import { GatsbyImage,getImage } from 'gatsby-plugin-image'
 
-export default function Project() {
+export default function Project({node}) {
+    console.log(node)
+    const image = getImage(node.src)
     return (
         <div className={project}>
             <div className={item1}>
-                <h1>WebStories</h1>
+                <h1>{node.title}</h1>
                 <div>
-                    <a href="https://github.com/pemba1s1/WebStories" target="_blank" rel="noreferrer"><box-icon type='logo' name='github'></box-icon></a>
-                    <a href="https://web-stories.herokuapp.com/" target="_blank" rel="noreferrer"><box-icon name='link-external' ></box-icon></a>
+                    <a href={node.repo} target="_blank" rel="noreferrer"><box-icon type='logo' name='github'></box-icon></a>
+                    <a href={node.demo} target="_blank" rel="noreferrer"><box-icon name='link-external' ></box-icon></a>
                 </div>
             </div>
             <div className={item1}>
+                <GatsbyImage image={image} alt={node.title} />
             </div>
             <div className={item2}>
-                <p>A place for reading, writing and sharing short stories and novels with other people.</p>
+                <p>{node.description}</p>
                 <br/>
-                <p className={tech}>Php</p>
-                <p className={tech}>MySql</p>
+                {node.techs.map(techh=>(
+                    <p className={tech}>{techh}</p>
+                ))}
             </div>
         </div>
     )
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,15 +1,36 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import Project from '../../components/Project'
 
 export default function index({data}) {
-    console.log(data)
     return (
         <div>
             <Layout>
             <title>Projects | Pemba</title>
-            <Project data={data}/>
+            {data.allProjectsJson.nodes.map(node=>(
+                <Project node={node} />
+            ))}
+            
             </Layout>
         </div>
     )
 }
+export const query = graphql`
+query MyQuery {
+    allProjectsJson {
+      nodes {
+        title
+        techs
+        description
+        repo
+        demo
+        src {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  } 
+`
